b96f9ce4034023af74180be832691ce52afae030
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-20 14:24:26 -0400

    Added functionality to operations event

--- ViktorBilyk-CalculatorApp-MAPD714/ViewController.swift
+++ ViktorBilyk-CalculatorApp-MAPD714/ViewController.swift
@@ -11,9 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     var isDotClick:Bool = false //checks if dot is clicked
+    var isMath:Bool = false //checks if operatiom button is clicked
+    var currentOperation:Int = 0 //stores tag of current operation
     var onScreenNumber:Double = 0 // number shown on screen [used for calculations]
     var resultNumber:Double = 0 // result of calculations
-    
     
     @IBOutlet weak var displayCalc: UILabel!
     
@@ -29,22 +30,63 @@ class ViewController: UIViewController {
         // clear clicked
         displayCalc.text = "0"
         isDotClick = false
+        isMath = false
+        currentOperation = 0
+        resultNumber = 0
+        onScreenNumber = 0
     }
     
     @IBAction func numbersClick(_ sender: UIButton) {
         // NumPad press
-        if ( displayCalc.text == "0")  {
+        if isMath {
             displayCalc.text = String(sender.tag-1)
+            isMath = false
         }
         else {
-            displayCalc.text = displayCalc.text! + String(sender.tag-1)
+            if ( displayCalc.text == "0")  {
+                displayCalc.text = String(sender.tag-1)
+            }
+            else {
+                displayCalc.text = displayCalc.text! + String(sender.tag-1)
+            }
         }
         onScreenNumber = Double(displayCalc.text!)!
     }
     
     @IBAction func operatorClick(_ sender: UIButton) {
         // Operation button clicked
-        
+        if (sender.tag != 15) {
+            resultNumber = onScreenNumber
+            isMath = true
+            currentOperation = Int(sender.tag)
+        }
+        else {
+            //equals pressed
+            //if (isMath) {
+                if (currentOperation == 11) {
+                    //divide
+                    resultNumber = resultNumber / onScreenNumber
+                }
+                else if (currentOperation == 12) {
+                    //multiply
+                    resultNumber = resultNumber * onScreenNumber
+                }
+                else if (currentOperation == 13) {
+                    //minus
+                    resultNumber = resultNumber - onScreenNumber
+                }
+                else if (currentOperation == 14) {
+                    //plus
+                    resultNumber = resultNumber + onScreenNumber
+                }
+            //}
+            //else {
+            //    resultNumber = onScreenNumber
+            //}
+            displayCalc.text = String(resultNumber)
+            //onScreenNumber = resultNumber
+            isDotClick = true
+        }
     }
     
     override func viewDidLoad() {
