38d2735cdc844a61d9ddc19bc5b38ef2bca6baea
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-20 11:01:36 -0400

    Initial Commit

---
+++ ViktorBilyk-CalculatorApp-MAPD714/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViktorBilyk-CalculatorApp-MAPD714
+//
+//  Created by Viktor Bilyk on 2017-09-20.
+//  Copyright © 2017 Shifty Land LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
